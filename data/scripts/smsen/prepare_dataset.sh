2e5393546fd9c5780426ac10c501df9c2b12a967
Martin Fischer <martin.fischer@stud.sbg.ac.at>
2022-07-04 12:19:10 +0200

    clean up shell script for SMSen dataset

--- data/scripts/smsen/prepare_dataset.sh
+++ data/scripts/smsen/prepare_dataset.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/bash
 
 DATASET_URL="git@frosch.cosy.sbg.ac.at:datasets/json/smsen.git"
-DATASET_PATH="../../datasets/smsen"
-DATASET="$DATASET_PATH/smsen.json"
+DATASET_PATH="$( dirname $0 )/../../datasets/smsen"
 
 if [ ! -d "$DATASET_PATH" ]; then
     git clone $DATASET_URL $DATASET_PATH
